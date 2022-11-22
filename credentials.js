6af0fc607a9d2a1240089f7adee2c994504a14b3
20220552 <111052401+20220552@users.noreply.github.com>
2022-11-22 22:26:58 +1300

    Delete credentials.js

--- credentials.js
+++
@@ -1,5 +0,0 @@
-export const SPACE_ID = "dgpo6cqzt962";
-export const ACCESS_TOKEN = "1RQEPbsg_MvNznBjdm5GLcSzmIYiMNAnNgJtXAqOBc4";
-export const ACCESS_TOKEN1 = "7jRsBovwqeIjIjRgcjvtFQVIXDleQmW8JHiHNcLUQdM";
-
-
